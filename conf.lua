e30af0acd2e33089e4e81815858538e4a6c9a0e3
Chenzo46 <85902885+Chenzo46@users.noreply.github.com>
2025-12-13 15:04:41 -0500

    hid console

--- conf.lua
+++ conf.lua
@@ -1,12 +1,11 @@
 function love.conf(t)
-    t.identity = "data/saves"
     t.gammacorrect = true
-    t.window.title = "Wowzers: Love Edition"
+    t.window.title = "WOWZERS: Love Edition"
     t.window.width = 360
     t.window.height = 640
     --t.window.borderless = true
     t.window.vsync = 0
-    t.console = true
+    --t.console = true
     --t.window.display = 1
     --t.window.fullscreen = true
 end
